c5e73d1ba320abd96fbd9145810b154e43a1f0f6
Antonio Vazquez-Mackay <73569624+chunkychund@users.noreply.github.com>
2022-11-14 12:09:08 -0800

    better test doc

--- utils/test_shipments.js
+++ utils/test_shipments.js
@@ -1,7 +1,7 @@
 export const TEST_ITEMS = [
     {
         shipment_id: 123,
-        price: 91.99,
+        price: 161.68,
         status: 1,
         priority: 0,
         date: "2021-10-20",
@@ -24,8 +24,7 @@ export const TEST_ITEMS = [
     },
     {
         shipment_id: 456,
-        contents: new Array(2).fill("Item 2"),
-        price: 5.99,
+        price: 91.99,
         status: 1,
         priority: 1,
         date: "2021-08-20",
@@ -43,12 +42,25 @@ export const TEST_ITEMS = [
         shipment_id: 999,
         contents: "Item 2, Item 3",
         price: 10.99,
-        priority: 2
+        status: 0,
+        priority: 2,
+        content: [
+            { 
+                item_id: 2,
+                name: "item2",
+                quantity: 1,
+                price: 10.99,
+            }
+        ]
+
     }
     ,
     {
-        shipment_id: 999,
+        shipment_id: 420,
         contents: "Item 2, Item 3",
-        price: 10.99,
+        price: 0,
+        status: 3,
+        priority: 2
+
     }
 ];
